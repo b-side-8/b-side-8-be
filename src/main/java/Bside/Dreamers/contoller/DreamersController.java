063c51ed0ca00e9bfd54e045d83303a48f0d59d8
ohasis <dhgkdud4002@naver.com>
2023-05-28 22:39:30 +0900

    main Feat : jenkins build test

--- src/main/java/Bside/Dreamers/contoller/DreamersController.java
+++ src/main/java/Bside/Dreamers/contoller/DreamersController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DreamersController {
     @GetMapping("hello")
     public String hello(Model model) {
+        //jenkins push test
 
         return "/hello";
     }
