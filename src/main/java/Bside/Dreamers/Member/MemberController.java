023a214a1dc4c6caef8961dd1e372a03edd3aea1
Hayoung <dhgkdud4002@dahanit.co.kr>
2023-07-07 18:02:13 +0900

    main Feat : 회원 등록시 정보 받아오기

--- src/main/java/Bside/Dreamers/Member/MemberController.java
+++ src/main/java/Bside/Dreamers/Member/MemberController.java
@@ -1,18 +1,29 @@
 package Bside.Dreamers.Member;
 
 import Bside.Dreamers.domin.Member;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
-    @GetMapping("/")
-    public void registMember(@RequestParam String nickName) throws Exception{
+    @ApiOperation(value = "회원 정보 저장")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="nickName", value = "닉네임", required = true, paramType = "query"),
+            @ApiImplicitParam(name="agreeYn", value = "약관동의여부", required = true, paramType = "query"),
+            @ApiImplicitParam(name="gender", value = "성별", required = false, paramType = "query"),
+            @ApiImplicitParam(name="birth", value = "출생연도", required = false, paramType = "query"),
+    })
+    @PostMapping("/join")
+    public void joinMember(@RequestBody Member member) throws Exception{
 
+        System.out.println("!!");
 
     }
 
