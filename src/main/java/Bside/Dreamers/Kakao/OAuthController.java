da878897bbcfff31b67b583d355da1f981bd14f1
Hayoung <dhgkdud4002@dahanit.co.kr>
2023-07-17 14:46:50 +0900

    ohasis Feat : 카카오 회원가입시 전달받은 id 값 query string으로 redirect

--- src/main/java/Bside/Dreamers/Kakao/OAuthController.java
+++ src/main/java/Bside/Dreamers/Kakao/OAuthController.java
@@ -1,11 +1,11 @@
 package Bside.Dreamers.Kakao;
 
-import Bside.Dreamers.domin.Member;
 import Bside.Dreamers.service.KaKaoUser;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
 @RestController
@@ -24,13 +24,17 @@ public class OAuthController {
      */
     @ResponseBody
     @GetMapping("/kakao")
-    public String kakaoCallback(@RequestParam String code) throws Exception{
+    public void kakaoCallback(@RequestParam String code, HttpServletResponse response) throws Exception{
         System.out.println(code);
         String accessToken = oAuthService.getKakaoAccessToken(code);
         HashMap<String, Object> member = kaKaoUser.createKakaoUser(accessToken);
 
         System.out.println(member.get("nickname"));
 
-        return (String)member.get("id");
+        String id = String.valueOf(member.get("id"));
+
+
+        String redirect_uri="http://localhost:3000/login/nickname?id="+id;
+        response.sendRedirect(redirect_uri);
     }
 }
