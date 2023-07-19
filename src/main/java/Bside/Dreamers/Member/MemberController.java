0d50d6f04379edc4e7b1f1702ea1039b65075afe
Hayoung <dhgkdud4002@dahanit.co.kr>
2023-07-19 14:40:47 +0900

    ohasis Feat : 회원정보 조회 api 추가

--- src/main/java/Bside/Dreamers/Member/MemberController.java
+++ src/main/java/Bside/Dreamers/Member/MemberController.java
@@ -7,10 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -31,4 +28,10 @@ public class MemberController {
         return new ResponseEntity<>(member, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "회원 정보 조회")
+    @GetMapping("/getMemberInfo")
+    public ResponseEntity<Member> getMemberInfo(@RequestParam("id") Long id){
+        Member member = memberService.getMemberInfo(id);
+        return ResponseEntity.ok(member);
+    }
 }
